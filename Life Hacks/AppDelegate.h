c3d034c59e1dc839d93c086279b72040ad24be77
case <case@caseys-macbook-pro.local>
2016-08-21 09:12:56 -0500

    Initial Commit

---
+++ Life Hacks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Life Hacks
+//
+//  Created by Casey Ross on 8/21/16.
+//  Copyright © 2016 Case Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Life Hacks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Life Hacks
+//
+//  Created by Casey Ross on 8/21/16.
+//  Copyright © 2016 Case Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
